57c36ddee9d1ae7f384cad487014f6fc059a33c6
leoTlr <leo.teuchtler@hotmail.de>
2018-07-16 14:20:19 +0200

    remove expand_key() and only iterate over key%len(key)

--- vigenere.cpp
+++ vigenere.cpp
@@ -11,7 +11,6 @@ const string alph = "abcdefghijklmnopqrstuvwxyz";
 std::array<string, 26> subst_table;
 
 unique_ptr<string> prepare_str(const string*);
-unique_ptr<string> expand_key(const unique_ptr<string>, const int);
 
 int main(int argc, char *argv[]){
 
@@ -20,7 +19,7 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     } // TODO: more failsafe argument checking
 
-    const string raw_key = string(argv[1]);
+    const string raw_key = string(argv[1]); // TODO: also use smart pointers
     const string raw_msg = string(argv[2]);
 
     if (raw_key.length() > raw_msg.length()){
@@ -28,16 +27,16 @@ int main(int argc, char *argv[]){
     } 
 
     const unique_ptr<string> msg = prepare_str(&raw_msg);
-    const unique_ptr<string> expanded_key = expand_key(prepare_str(&raw_key), msg->length());
+    const unique_ptr<string> key = prepare_str(&raw_key);
 
-    cout << "key: " << *expanded_key << endl;
+    cout << "key: " << *key << endl;
     cout << "msg: " << *msg << endl;
     
     // build substitution-alphabets (ROT-n, 1<=n<=25)
+    char pos;
     subst_table[0] = alph;
     for (auto it = alph.begin()+1; it < alph.end(); it++){
-        char c = *it;
-        int pos = alph.find(c);
+        pos = *it - 'a';
         string subst_alph = alph.substr(pos, alph.length());
         subst_alph += alph.substr(0, pos);
         subst_table[pos] = subst_alph;
@@ -47,14 +46,14 @@ int main(int argc, char *argv[]){
     string cipher_message = "";
     cipher_message.reserve(msg->size());
 
-    // build cipher message
-    char row, col, i, key_char;
+    // translate char for char in in msg to cipher_msg
+    char row, col, key_pos, key_char;
     for (char msg_char: *msg){
-        key_char = expanded_key->at(i);
-        row = alph.find(key_char);
-        col = alph.find(msg_char);
+        key_char = key->at(key_pos);
+        row = key_char - 'a'; // ASCII(lower) char -'a' -> 0-25
+        col = msg_char - 'a';
         cipher_message += subst_table[row][col];
-        i++;
+        key_pos = (++key_pos)%key->length(); // iterate over key for as long as needed
     }
 
     cout << cipher_message << endl;
@@ -62,44 +61,31 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-unique_ptr<string> expand_key(const unique_ptr<string> key, const int msg_len){
-    // need key with length == msg-length
-    // repeat key as often as needed
-
-    int i;
-    unique_ptr<string> expanded_key = std::make_unique<string>("");
-    expanded_key->reserve(msg_len);
-    for (i=0; i<msg_len; i++){
-        *expanded_key += (*key)[i%key->length()];
-    }
-    return expanded_key;
-}
-
 unique_ptr<string> prepare_str(const string* str){
     // convert string to only ascii-lowercase letters
 
-    unique_ptr<string> str_p = std::make_unique<string>(*str);
+    unique_ptr<string> ret_str = std::make_unique<string>(*str);
 
-    if (str_p->empty()){
+    if (ret_str->empty()){
         cerr << "empty argument. exiting" << endl;
         exit(EXIT_FAILURE);
     }
 
     // make lowercase
-    std::transform(str_p->begin(), str_p->end(), str_p->begin(), tolower);
+    std::transform(ret_str->begin(), ret_str->end(), ret_str->begin(), tolower);
 
     // remove non-(lowercase)letters (ASCII)
     auto is_not_ascii_lower = [](char c){ return !((c>=97)&&(c<=122)); };
-    str_p->erase(std::remove_if(str_p->begin(), str_p->end(), is_not_ascii_lower), str_p->end());
+    ret_str->erase(std::remove_if(ret_str->begin(), ret_str->end(), is_not_ascii_lower), ret_str->end());
 
-    if (!str_p) {
+    if (!ret_str) {
         cerr << "error during key/message preparation" << endl;
         exit(EXIT_FAILURE);
     }
-    if (str_p->empty()){
+    if (ret_str->empty()){
         cerr << "empty string after removing non-letters. exiting" << endl;
         exit(EXIT_FAILURE);
     }
 
-    return str_p;
+    return ret_str;
 }
